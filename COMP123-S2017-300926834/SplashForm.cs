acb7125b0b244a1f01b78910e705887e414f70d6
300926834 <300926834@PR-A315-23>
2017-08-17 13:00:25 -0400

    changed properties of the splashform with timer

--- COMP123-S2017-300926834/SplashForm.cs
+++ COMP123-S2017-300926834/SplashForm.cs
@@ -16,5 +16,10 @@ namespace COMP123_S2017_300926834
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
     }
 }
